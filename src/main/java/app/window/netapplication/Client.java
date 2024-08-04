3b41f75035f3483118dd72ac4581d4ada0570362
Yato <kkent7826@gmail.com>
2024-08-04 19:05:09 -0400

    Receiving packets/GUI changes

--- src/main/java/app/window/netapplication/Client.java
+++ src/main/java/app/window/netapplication/Client.java
@@ -2,20 +2,27 @@ package app.window.netapplication;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.*;
+
 
 public class Client {
     private final String m_Name;
     private final String m_Address;
-    private final String m_Port;
+    private final Integer m_Port;
 
     private Scene m_Scene;
 
@@ -23,52 +30,105 @@ public class Client {
     private TextField m_UserInput;
     private Button m_Button;
 
-    public Client(String name, String address, String port) {
+    private DatagramSocket m_Socket;
+    private InetAddress ip;
+
+    public Client(String name, String address, Integer port) {
         m_Name = name;
         m_Address = address;
         m_Port = port;
 
-        createWindow();
+        boolean connect = openConnection(address, port);
+        if (!connect) {
+            System.err.println("Connection failed!");
+        }
+        try {
+            createWindow();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private void createWindow() {
-        Stage stage = new Stage();
+    private boolean openConnection(String address, int port) {
+        try {
+            m_Socket = new DatagramSocket(port);
+            ip = InetAddress.getByName(address);
+        } catch (UnknownHostException | SocketException e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
 
+        return true;
+    }
+
+    private String receive() {
+        byte[] data = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+
+        try {
+            m_Socket.receive(packet);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return new String(packet.getData());
+    }
+
+    private void createWindow() throws IOException {
+        Stage clientStage = new Stage();
+
+        // Chat area
         m_TextArea = new TextArea();
         m_TextArea.setEditable(false);
+        m_TextArea.setWrapText(true);
         m_TextArea.setPrefWidth(800);
+        m_TextArea.setPrefHeight(600);
+        m_TextArea.addEventFilter(KeyEvent.ANY, event -> m_UserInput.requestFocus());
 
-        m_UserInput = new TextField();
-
+        // Button
         m_Button = new Button();
         m_Button.setText("Send");
         m_Button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                System.out.println(m_Name + ": " + m_UserInput.getText());
-                console(m_UserInput.getText());
+                if (!m_UserInput.getText().isEmpty())
+                    System.out.println(m_Name + ": " + m_UserInput.getText());
+                send(m_UserInput.getText());
                 m_UserInput.clear();
             }
         });
+        m_Button.addEventFilter(MouseEvent.MOUSE_RELEASED, event -> m_UserInput.requestFocus());
+        m_Button.addEventFilter(KeyEvent.ANY, event -> m_UserInput.requestFocus());
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.TOP_LEFT);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.addColumn(0, m_TextArea, m_UserInput);
-        grid.addRow(1, m_Button);
+        // Field for user input
+        m_UserInput = new TextField();
 
-        m_Scene = new Scene(grid, 800, 600);
+        m_UserInput.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                m_Button.fire();
+            }
+        });
 
-        stage.setTitle("Chat Application");
-        stage.setScene(m_Scene);
-        stage.show();
+        HBox hBox = new HBox(10, m_UserInput, m_Button);
+        HBox.setHgrow(m_UserInput, Priority.ALWAYS);
+        hBox.setPadding(new Insets(10, 0, 0, 0));
+
+        VBox vbox = new VBox(m_TextArea, hBox);
+        VBox.setVgrow(m_TextArea, Priority.ALWAYS);
+        vbox.setPadding(new Insets(10));
+
+        m_Scene = new Scene(vbox);
+
+        clientStage.setTitle("Chat Application");
+        clientStage.setScene(m_Scene);
+        clientStage.show();
+
+        m_UserInput.requestFocus();
     }
 
-    private void console(String message) {
-        if (message.equals(""))
+    private void send(String message) {
+        if (message.isEmpty())
             return;
         m_TextArea.appendText(m_Name + ": " + message + "\n\r");
-        m_UserInput.clear();
     }
 }
--- src/main/java/app/window/netapplication/Login.java
+++ src/main/java/app/window/netapplication/Login.java
@@ -9,45 +9,72 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.converter.IntegerStringConverter;
 
-import java.io.IOException;
+import java.util.function.UnaryOperator;
 
 
 public class Login extends Application {
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        Label userId = new Label("Name");
-        final TextField userName = new TextField();
+    private TextField userName, ipAddress, Port;
+    private Label userLabel, addressLabel, portLabel;
+    private Label errorLabel;
+    private Button submitButton;
 
-        Label address = new Label("IP Address");
-        final TextField ipAddress = new TextField();
+    public void Init() {
+        userLabel = new Label("Name");
+        userName = new TextField();
+
+        addressLabel = new Label("IP Address");
+        ipAddress = new TextField();
         ipAddress.setPromptText("192.168.0.1");
 
-        Label port = new Label("Port");
-        final TextField Port = new TextField();
+        // Accept only integers in port field
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("-?\\d*"))
+                return change;
+            return null;
+        };
+
+        portLabel = new Label("Port");
+        Port = new TextField();
         Port.setPromptText("6969");
+        TextFormatter<Integer> formatter = new TextFormatter<>(new IntegerStringConverter(), null, filter);
+        Port.setTextFormatter(formatter);
 
-        Button btn = new Button();
+        submitButton = new Button();
+        submitButton.setText("Submit");
 
-        HBox hbBtn = new HBox(10);
-        hbBtn.getChildren().add(btn);
-        hbBtn.setAlignment(Pos.BOTTOM_CENTER);
+        errorLabel = new Label("");
 
-        btn.setText("Submit");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        setFieldKeyEvents();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Init();
+
+        submitButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
+                if (!validateCredentials())
+                    return;
+
                 System.out.println("Name: " + userName.getText());
                 System.out.println("Address: " + ipAddress.getText());
-                System.out.println("Port: " + Port.getText());
+                System.out.println("Port: " + Port.getText() + "\n");
 
                 String name = userName.getText();
                 String address = ipAddress.getText();
-                String port = Port.getText();
+                Integer port = Integer.valueOf(Port.getText());
 
                 // Close login window
                 stage.close();
@@ -57,16 +84,20 @@ public class Login extends Application {
             }
         });
 
+        VBox vBox = new VBox(10, submitButton, errorLabel);
+        vBox.setAlignment(Pos.BOTTOM_CENTER);
+
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        grid.addColumn(0, userId, userName);
-        grid.addColumn(0, address, ipAddress);
-        grid.addColumn(0, port, Port);
-        grid.addColumn(0, hbBtn);
+        grid.addColumn(0, userLabel, userName);
+        grid.addColumn(0, addressLabel, ipAddress);
+        grid.addColumn(0, portLabel, Port);
+        grid.addColumn(0, vBox);
+
 
         Scene scene = new Scene(grid, 300, 300);
         stage.setTitle("Chat Login");
@@ -74,8 +105,40 @@ public class Login extends Application {
         stage.show();
     }
 
-    private void login(String name, String address, String port) {
+    private void login(String name, String address, Integer port) {
         new Client(name, address, port);
+    }
+
+    private void setFieldKeyEvents() {
+        userName.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                submitButton.fire();
+            }
+        });
+
+        ipAddress.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                submitButton.fire();
+            }
+        });
+
+        Port.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                submitButton.fire();
+            }
+        });
+    }
+
+    private boolean validateCredentials() {
+        if (userName.getText().isEmpty() ||
+            ipAddress.getText().isEmpty() ||
+            Port.getText().isEmpty())
+        {
+            errorLabel.setText("Fill empty fields!");
+            return false;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
